c7907d8e4665859f9621ef54aa6532664620048d
thecosmos <deepakrajko14@gmail.com>
2019-01-07 21:50:11 +0530

    Meme processor core

---
+++ app/core/image/functions/memes/__init__.py
@@ -0,0 +1,7 @@
+from .memes import *
+
+
+class MemesProcessor(object):
+
+    def __init__(self):
+        self.rip = RIPMeme()
---
+++ app/core/image/functions/memes/models.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from PIL import Image
+
+
+class Meme(ABC):
+
+    FONT_PATH = "app/core/image/templates/fonts/"
+
+    @property
+    @abstractmethod
+    def BASE_MEME_PATH(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _process(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def meme(self, text: str, avatar: Image) -> Image:
+        raise NotImplementedError
+
+    def get_base_meme(self) -> Image:
+        return Image.open(self.BASE_MEME_PATH, mode="r")
