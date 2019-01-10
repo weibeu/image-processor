e8fc2f38b340ca9335a7cdd1d9a7e561d8ff2831
thecosmos <deepakrajko14@gmail.com>
2019-01-10 21:37:47 +0530

    No more need to cache

--- app/core/server/api/misc/memes.py
+++ app/core/server/api/misc/memes.py
@@ -10,16 +10,16 @@ from app.core.image.processor import ImageProcessor
 class RIPMeme(Resource):
 
     MEME_NAME = "rip.png"
-    FLASK_BASE_NAME = "static/image/cache/PIL/meme.png"
-    BASE_NAME = "app/core/server/" + FLASK_BASE_NAME
 
     def get(self, text, avatar_url):
         response = requests.get(avatar_url)
         avatar_bytes = BytesIO(response.content)
         meme = ImageProcessor().memes.rip.meme(text, avatar_bytes)
-        meme.save(self.BASE_NAME)
+        meme_bytes = BytesIO()
+        meme.save(meme_bytes, format="PNG")
+        meme_bytes.seek(0)
         return send_file(
-            self.FLASK_BASE_NAME,
+            meme_bytes,
             mimetype="image/png",
             attachment_filename=self.MEME_NAME
         )
