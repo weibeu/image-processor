5b6e5d453f3a5685f3964dacccc07804ba31f16d
thecosmos <deepakrajko14@gmail.com>
2019-01-10 22:52:37 +0530

    Removed unwanted stuffs and clean basic API

--- app/core/server/index.py
+++ app/core/server/index.py
@@ -1,17 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_restful import Api
 
-from .api.misc.memes import RIPMeme
-from .api.resources import RandomEmote
+from .api.resources.image_processor.memes import RIPMeme
 
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(RandomEmote, '/random_emote/')
 api.add_resource(RIPMeme, '/rip_meme/<string:text>/<path:avatar_url>/')
-
-
-@app.route('/')
-def index():
-    return render_template("index.html")
