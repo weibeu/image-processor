9d4bdd584a2cebb2f2e173d4f8dc51d70ade208e
thecosmos <deepakrajko14@gmail.com>
2019-04-02 16:21:43 +0530

    Include banner tests only atm

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import unittest
 from typing import Union
 
-from _tests.image_processing.memes import MemesTest
-from _tests.image_processing.discord import BannerTest, MessageScreenshotsTest
+# from _tests.image_processing.memes import MemesTest
+from _tests.image_processing.discord import BannerTest #, MessageScreenshotsTest
 
 
 if __name__ == "__main__":
