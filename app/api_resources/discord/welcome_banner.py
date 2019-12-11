f2cb135dcf5e25f8916b0f16f23c5fa8a18db994
thecosmos <deepakrajko14@gmail.com>
2019-12-12 03:00:36 +0530

    TODO

--- app/api_resources/discord/welcome_banner.py
+++ app/api_resources/discord/welcome_banner.py
@@ -71,7 +71,7 @@ class WelcomeBanner(ApiResourceBase):
         # avatar = self.add_avatar_border(avatar)
         avatar_mask = Image.new("L", avatar.size)
         avatar_drawer = ImageDraw.Draw(avatar_mask)
-        avatar_drawer.ellipse((0, 0) + avatar.size, fill=225)
+        avatar_drawer.ellipse((0, 0) + avatar.size, fill=225)    # TODO: use payload.get("avatar_border_color").
         avatar = ImageOps.fit(avatar, avatar_mask.size)
         avatar.putalpha(avatar_mask)
 
